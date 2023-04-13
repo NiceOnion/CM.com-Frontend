4adf89536b4f6121d7f2aee9f43d0a14dfb6f6ef
Saeryun3 <samed19982008@hotmail.com>
2023-04-13 11:41:15 +0200

    Edit demo href

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import{EditDemoComponent} from "./edit-demo/edit-demo.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'edit-demo/:id', component: EditDemoComponent },
   // Add any other routes here
 ];
 
