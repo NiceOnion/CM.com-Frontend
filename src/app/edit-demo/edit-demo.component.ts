944198fa6ffbc42af6f5316537fbb97560f1fbce
Saeryun3 <samed19982008@hotmail.com>
2023-04-13 11:20:23 +0200

    Edit-demo initiallize

---
+++ src/app/edit-demo/edit-demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-demo',
+  templateUrl: './edit-demo.component.html',
+  styleUrls: ['./edit-demo.component.css']
+})
+export class EditDemoComponent {
+
+}
