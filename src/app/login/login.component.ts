b8dadb1f3eb6242d72972442fa45f7746995ad82
Amin El Abdellaoui <i467072@fhict.nl>
2023-04-17 10:29:22 +0200

    Hotfixes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,42 @@
 import { Component } from '@angular/core';
-import {ApiService} from "../api.service";
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  name: string = "";
-  password: string = "";
+  name: string = '';
+  password: string = '';
+  incorrectTxt: string = '';
   user: User[] = [];
-  constructor(private ApiService: ApiService) {
+  constructor(private ApiService: ApiService, private router: Router) {}
+  loginClick(): void {
+    this.ApiService.login(this.name, this.password).subscribe(
+      (data: any) => {
+        if (data == 0) {
+          console.log('Lmao');
+        } else {
+          localStorage.setItem('currentUserId', data);
+          this.router.navigate(['']);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-  loginClick ():void{
-    this.ApiService.login(this.name, this.password).subscribe((data : any) => {console.log(data);}, error => {console.log(error)});
+  show: boolean = false;
+
+  inputTypes() {
+    this.show = !this.show;
   }
 }
 
-class User{
+class User {
   id: number = 0;
-  name : string = "";
-  password: string = "";
+  name: string = '';
+  password: string = '';
 }
