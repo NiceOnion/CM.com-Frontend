440e3d92fdc77ed58e7e6f375919f04883943e07
Amin El Abdellaoui <i467072@fhict.nl>
2023-06-12 10:18:04 +0200

    Sessions changed to cookies.

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css'],
+  providers: [CookieService]
 })
 export class ToolbarComponent {
-  constructor(private ApiService: ApiService, private router: Router) {}
+  constructor(private ApiService: ApiService, private router: Router, private cookieService: CookieService) {}
   //Retrieves the username stored in the cookies
   getName() {
-    return sessionStorage.getItem('currentUserName');
+    if(this.cookieService.get('currentUserName') == ""){
+      return null;
+    }
+    else{
+      return this.cookieService.get('currentUserName');
+    }
   }
   //If the user is logged in, the icon button will redirect to the home page. Otherwise, it will return to the login page
   iconButton(): string{
-    if(sessionStorage.getItem('currentUserName') != null){
+    if(this.cookieService.get('currentUserName') != ""){
       return "";
     }
     else{
@@ -24,8 +31,7 @@ export class ToolbarComponent {
   }
   //A function that clears all the user's cookies and returns the login page
   logOut(): void {
-    localStorage.clear();
-    sessionStorage.clear();
+    this.cookieService.deleteAll();
     this.router.navigate(['login']);
   }
 }
